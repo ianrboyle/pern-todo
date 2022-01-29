efe829c161e8080088ce7d3e163b02ade4cdaf01
Ian Boyle <irboyle21@gmail.com>
2022-01-29 18:45:52 -0500

    adds logging.ts file

---
+++ source/config/logging.ts
@@ -0,0 +1,41 @@
+import { get } from 'mongoose';
+
+const getTimeStamp = (): string => {
+    return new Date().toISOString();
+};
+
+const info = (namespace: string, message: string, object?: any) => {
+    if (object) {
+        console.log(`[${getTimeStamp}] [INFO] [${namespace}] ${message}`, object);
+    } else {
+        console.log(`[${getTimeStamp}] [INFO] [${namespace}] ${message}`);
+    }
+};
+const warn = (namespace: string, message: string, object?: any) => {
+    if (object) {
+        console.warn(`[${getTimeStamp}] [WARN] [${namespace}] ${message}`, object);
+    } else {
+        console.warn(`[${getTimeStamp}] [WARN] [${namespace}] ${message}`);
+    }
+};
+const debug = (namespace: string, message: string, object?: any) => {
+    if (object) {
+        console.debug(`[${getTimeStamp}] [DEBUG] [${namespace}] ${message}`, object);
+    } else {
+        console.debug(`[${getTimeStamp}] [DEBUG] [${namespace}] ${message}`);
+    }
+};
+const error = (namespace: string, message: string, object?: any) => {
+    if (object) {
+        console.error(`[${getTimeStamp}] [ERRPR] [${namespace}] ${message}`, object);
+    } else {
+        console.error(`[${getTimeStamp}] [ERRPR] [${namespace}] ${message}`);
+    }
+};
+
+export default {
+    info,
+    warn,
+    error,
+    debug
+};
