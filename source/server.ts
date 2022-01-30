087eeb6bbfaa777da96a8522792a5f7873a81c52
Ian Boyle <irboyle21@gmail.com>
2022-01-29 19:01:58 -0500

    adds server.ts file

---
+++ source/server.ts
@@ -0,0 +1,52 @@
+import http from 'http';
+import express from 'express';
+import logging from './config/logging';
+import config from './config/config';
+import bodyParser from 'body-parser';
+// import bookRoutes from './routes/book';
+
+const NAMESPACE = 'Server';
+const router = express();
+
+/** Logging the request */
+
+router.use((req, res, next) => {
+    logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]`);
+
+    res.on('finish', () => {
+        logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}], STATUS - [${req.statusCode}]`);
+    });
+});
+
+router.use(bodyParser.urlencoded({ extended: false }));
+// removes need to call json.parse
+router.use(bodyParser.json());
+
+/** Rules of API */
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
+/** Routes go here */
+// router.use('/books', bookRoutes);
+
+/** Error handling */
+router.use((req, res, next) => {
+    const error = new Error('Not found');
+
+    res.status(404).json({
+        message: error.message
+    });
+});
+
+const httpServer = http.createServer(router);
+
+httpServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server is running ${config.server.hostname}:${config.server.port}`));
