c148314702d8441f06afac9145b884e8f22de76f
Ian Boyle <irboyle21@gmail.com>
2022-01-31 08:24:49 -0500

    adds update todo route

--- source/index.ts
+++ source/index.ts
@@ -46,9 +46,19 @@ app.get('/todos/:id', async (req, res) => {
     }
 });
 
-// get TODO
-
 // update TODO
+
+app.put('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+        const updateTodo = await pool.query('UPDATE todo SET description = $1 WHERE todo_id  = $2', [description, id]);
+
+        res.json('Todo was updated');
+    } catch (err: any) {
+        console.error(err.message);
+    }
+});
 
 //delete todos
 app.listen(5000, () => {
