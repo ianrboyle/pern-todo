6fa99da8ab5c6de0be8397383ed13864aa878197
Ian Boyle <irboyle21@gmail.com>
2022-01-31 08:20:33 -0500

    adds get todo by id route

--- source/index.ts
+++ source/index.ts
@@ -33,6 +33,19 @@ app.get('/todos', async (req, res) => {
     }
 });
 
+//get TODO by ID
+app.get('/todos/:id', async (req, res) => {
+    try {
+        //console.log(req.params) ==> logs the params (:id)
+        const { id } = req.params;
+
+        const todo = await pool.query('SELECT * FROM todo WHERE todo_id  = $1', [id]);
+        res.json(todo.rows[0]);
+    } catch (err: any) {
+        console.error(err.message);
+    }
+});
+
 // get TODO
 
 // update TODO
