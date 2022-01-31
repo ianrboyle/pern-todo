701af08617896ce05a201a02e394bffce3e4f744
Ian Boyle <irboyle21@gmail.com>
2022-01-30 19:52:39 -0500

    adds db folder and adds post route in index.ts file

---
+++ source/db/database.sql
@@ -0,0 +1,10 @@
+-- CREATE DATABASE perntodo;
+
+CREATE TABLE todo(
+  todo_id SERIAL PRIMARY KEY,
+  description VARCHAR(255)
+)
+
+--make less rigid
+
+-- add additional fields to TODO's
--- source/index.ts
+++ source/index.ts
@@ -1,12 +1,31 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
+const pool = require('./db/db');
 // import bookRoutes from './routes/book';
 const app = express();
 const cors = require('cors');
 //middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//ROUTES
+
+//create TODO
+
+app.post('/todos', async (req, res) => {
+    try {
+        console.log(req.body);
+    } catch (err: any) {
+        console.error(err.message);
+    }
+});
+// get all TODOs
+
+// get TODO
+
+// update TODO
+
+//delete todos
 app.listen(5000, () => {
     console.log('server running on PORT 5000');
 });
